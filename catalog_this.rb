56bbf413e3c425931aaaf9dab7b48c07fa9d4af9
Guilherme Moretti <guilherme.moretti@innvent.com.br>
2014-05-26 00:05:27 -0300

    Using fakeredis for development/test env

--- catalog_this.rb
+++ catalog_this.rb
@@ -1,15 +1,25 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/base'
 require 'json'
 require 'redcarpet'
-require 'redis'
 require 'pry'
 require 'dotenv'
-
 require_relative 'app/cataloger'
+require_relative 'app/app_infos'
 
-Dotenv.load
+if ENV['RACK_ENV'] != 'production'
+  require 'fakeredis'
+else
+  require 'redis'
+end
+
+if ENV['RACK_ENV'] != 'production' ||
+  ENV['RACK_ENV'] != 'test'
+  Dotenv.load
+end
 
 class CatalogThis < Sinatra::Base
 
@@ -20,6 +30,16 @@ class CatalogThis < Sinatra::Base
   post '/catalog/' do
     link = Cataloger.catalog(params[:link])
     json link
+  end
+
+  get '/info/' do
+    info = AppInfos.get
+    json info
+  end
+
+  get '/search/' do
+    links = Cataloger.search(params)
+    json links
   end
 
 end
