d57925f638c34a7ce1fb891d7b16a744951b774b
Guilherme Moretti <guilherme.moretti@innvent.com.br>
2014-05-26 00:04:57 -0300

    Refactor redis_client.rb

--- app/models/redis_client.rb
+++ app/models/redis_client.rb
@@ -1,21 +1,28 @@
+require 'singleton'
+
 class RedisClient
-
-  def self.get_instance
-    @@instance = self.new
-  end
-
-  def initialize
-    @client ||= Redis.new(redis_conf)
-  end
+  include Singleton
 
   def set(name,data)
-    @client.set(name,data.to_json)
+    client.set(name,data.to_json)
   end
 
   def get(name)
-    rs = @client.get(name)
-    JSON.parse(rs) unless rs.nil?
+    rs = client.get(name)
+    return JSON.parse(rs) unless rs.nil?
     return false
+  end
+
+  def count_keys(prefix='*')
+    keys(prefix).count
+  end
+
+  def keys(prefix='*')
+    client.keys(prefix + '*')
+  end
+
+  def exists(key)
+    client.exists(key)
   end
 
   private
@@ -23,6 +30,11 @@ class RedisClient
     #https://devcenter.heroku.com/articles/rediscloud
     uri = URI.parse(ENV["REDISCLOUD_URL"])
     {:host => uri.host, :port => uri.port, :password => uri.password}
+  end
+
+  def client
+    @client ||= Redis.new(redis_conf)
+    @client
   end
 
 end
